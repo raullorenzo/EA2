c3ef28728c0b87bf88a8f2205f609a63d5a8a867
Carlos Valdes <antmotion@gmail.com>
2016-05-14 03:38:39 +0200

    acceso facebook general

--- config/auth.js
+++ config/auth.js
@@ -2,9 +2,9 @@ module.exports = {
 
     'facebookAuth':
     {
-        id: '203907273328129',
-        secret: '8f35db556a12680a940a8853d7c62b5d',
-        callbackURL:'http://localhost:3000/auth/facebook/callback',
+        id: '230153647346505',
+        secret: '0c0fa7e2cbe2dfb8481d9f2b3f8db65f',
+        callbackURL:'http://147.83.7.158:3000/auth/facebook/callback',
         profileFields: ['id','name','emails','photos']
 
     }
