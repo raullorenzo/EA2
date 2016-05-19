5881e60f9e78227678244120e6c033d57320f181
raullorenzo <raul.lorenzo.67@gmail.com>
2016-05-19 23:44:13 +0200

    historial2

--- routes/historiales.js
+++ routes/historiales.js
@@ -3,9 +3,38 @@ module.exports = function (app) {
     var mongoose = require('mongoose');
     var Historial = require('../modelos/historial.js');
 
-    ObtenerHistorialesL= function (req, res){
-        console.log('GET/ObtenerHistorialesLogin/'+ req.params.login);
+
+
+    // ObtenerHistorialesL= function (req, res){
+    //     console.log('GET/ObtenerHistorialesLogin/'+ req.params.login);
+    //     Historial.find({$or:[{logincreador:req.params.login},{logininvitado:req.params.login}]},function (err, historiales){
+    //         if (err) return res.send(500, err.message);
+    //             console.log(historiales);
+    //             res.status(200).jsonp(historiales);
+    //         });
+    // };
+
+    // ObtenerHistorialesL= function (req, res){
+    //     console.log('GET/ObtenerHistorialesLogin/max');
+    //     Historial.find({$or:[{logincreador:'max'},{logininvitado:'max'}]},function (err, historiales){
+    //         if (err) return res.send(500, err.message);
+    //             console.log(historiales);
+    //             res.status(200).jsonp(historiales);
+    //         });
+    // };
+
+    ObtenerHistorialesL2= function (req, res){
+        console.log('GET/ObtenerHistorialesLogin/' + req.query.login);
         Historial.find({$or:[{logincreador:req.query.login},{logininvitado:req.query.login}]},function (err, historiales){
+            if (err) return res.send(500, err.message);
+                console.log(historiales);
+                res.status(200).jsonp(historiales);
+            });
+    };
+
+    ObtenerHistorialesL= function (req, res){
+        console.log('GET/ObtenerHistorialesLogin/' + req.params.login);
+        Historial.find({$or:[{logincreador:req.params.login},{logininvitado:req.params.login}]},function (err, historiales){
             if (err) return res.send(500, err.message);
                 console.log(historiales);
                 res.status(200).jsonp(historiales);
@@ -63,5 +92,5 @@ module.exports = function (app) {
     };
 
     app.get('/historial/ObtenerHisorialesPaginados', ObtenerHistorialesP);
-    app.get('/historial/ObtenerHistorialesLogin', ObtenerHistorialesL);
+    app.get('/historial/ObtenerHistorialesLogin/:login/', ObtenerHistorialesL);
 }
