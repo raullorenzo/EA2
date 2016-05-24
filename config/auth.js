375497385f211ae22bf9c6ad8675a7c49b8ad1a8
Carlos Valdes <antmotion@gmail.com>
2016-05-24 02:52:01 +0200

    email facebook y usuarios activos cha

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     {
         id: '245833209111882',
         secret: '80790b4495db90d1798a26668ae485bd',
-        callbackURL:'http://localhost:3000/auth/facebook/callback',
+        callbackURL:'http://147.83.7.158:3000//auth/facebook/callback',
         profileFields: ['id','name','emails','photos']
 
     }
