e2976435806cbe9f92b9916585602336519bb68b
Carlos Valdes <antmotion@gmail.com>
2016-05-24 02:56:18 +0200

    face u acti

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     {
         id: '230153647346505',
         secret: '0c0fa7e2cbe2dfb8481d9f2b3f8db65f',
-        callbackURL:'http://147.83.7.158:3000//auth/facebook/callback',
+        callbackURL:'http://147.83.7.158:3000/auth/facebook/callback',
         profileFields: ['id','name','emails','photos']
 
     }
